eea3644f9c14e2dcb52cdd8f19c4d3226b2c4c14
AleahHunter16 <AleahHunter16@gmail.com>
2024-10-07 11:25:33 -0400

    commit

---
+++ JS_Intro/Functions/Index.js
@@ -0,0 +1,5 @@
+function computeArea(width: height) ;{// Calculate the area of the rectangle
+    const area = width * height;
+    // Return the result as a string return 'The area of a rectangle with a}
+    // Test the function by invoking it 
+    console.log(computeArea(5, 10))}
